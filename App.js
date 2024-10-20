f778c3fc9418b568de08af9063e8055b90f46728
selam24 <selamtessera@gmail.com>
2024-10-20 16:14:30 -0400

    updated APP.JS

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 //db connection credentials
-const dbConnection = require("./db/dbconfig");
+const dbConnection = require("./db/dbconfig.js");
 
 //   //testing the backend
 app.get("/", (req, res) => {
